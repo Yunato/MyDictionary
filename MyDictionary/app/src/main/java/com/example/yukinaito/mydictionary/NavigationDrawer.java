67bf528720c6a1457a082f38cd9883928d9bdeaf
Yunato <yunato525@gmail.com>
2018-10-17 19:20:53 +0900

    This commit show that the SelectClassActivity is transformed into SelectClassFragment.

--- MyDictionary/app/src/main/java/com/example/yukinaito/mydictionary/NavigationDrawer.java
+++ MyDictionary/app/src/main/java/com/example/yukinaito/mydictionary/NavigationDrawer.java
@@ -5,12 +5,16 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
-public class NavigationDrawer extends AppCompatActivity{
+public class NavigationDrawer extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
     private static final int REQUEST_WRITE_STORAGE = 1;
 
     @Override
@@ -24,13 +28,40 @@ public class NavigationDrawer extends AppCompatActivity{
         }
 
         NavigationView navigationView = (NavigationView)findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(MenuItem menuItem){
-                return false;
-            }
-        });
+        navigationView.setNavigationItemSelectedListener(this);
 
+        getSupportFragmentManager().popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        SelectClassFragment fragment = new SelectClassFragment();
+        transaction.add(R.id.main_layout, fragment);
+        transaction.commit();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        getSupportFragmentManager().popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        switch (id) {
+            case R.id.top_dictionary:
+                SelectClassFragment fragment = new SelectClassFragment();
+                transaction.replace(R.id.main_layout, fragment);
+                break;
+            case R.id.add_dictionary:
+                break;
+            case  R.id.top_research:
+                break;
+            case R.id.add_research:
+                break;
+            default:
+                return false;
+        }
+        transaction.commit();
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return false;
     }
 
     @Override
--- MyDictionary/app/src/main/java/com/example/yukinaito/mydictionary/SelectClassFragment.java
+++ MyDictionary/app/src/main/java/com/example/yukinaito/mydictionary/SelectClassFragment.java
@@ -1,38 +1,42 @@
 package com.example.yukinaito.mydictionary;
 
+import android.support.v4.app.ListFragment;
 import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
-public class SelectClassActivity extends AppCompatActivity {
+public class SelectClassFragment extends ListFragment {
     private static final int VIEW_CODE = 1;
     private static final int ADD_CODE = 2;
     //Activityの状態を示す。 true = 分野選択 false = 追加順
     private static boolean CONDITION = true;
+    String[] items;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_select_class);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.activity_select_class, container, false);
+    }
 
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState){
         DBAccess();
 
-        FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton)view.findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             //単語の追加
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),AddEditWordActivity.class);
+                Intent intent = new Intent(getActivity().getApplicationContext(),AddEditWordActivity.class);
                 startActivityForResult(intent, ADD_CODE);
             }
         });
@@ -40,30 +44,30 @@ public class SelectClassActivity extends AppCompatActivity {
 
     //DBへアクセスする 表示内容の更新
     public void DBAccess(){
-        SQLiteApplication sqLiteApplication = (SQLiteApplication)this.getApplication();
-        ListView listView = (ListView)findViewById(R.id.listView);
+        SQLiteApplication sqLiteApplication = (SQLiteApplication)getActivity().getApplication();
+        ListView listView = getListView();
 
         if(CONDITION) {
-            final String[] items = sqLiteApplication.getWordClass();
-            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.rowdata, items);
-            listView.setAdapter(adapter);
+            items = sqLiteApplication.getWordClass();
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), R.layout.rowdata, items);
+            setListAdapter(adapter);
 
             listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                 public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                    Intent intent = new Intent(getApplicationContext(), SelectWordActivity.class);
+                    Intent intent = new Intent(getActivity().getApplicationContext(), SelectWordActivity.class);
                     intent.putExtra("CLASS", items[position]);
                     startActivityForResult(intent, VIEW_CODE);
                 }
             });
         }else{
             final ArrayList<AdapterItem> items = sqLiteApplication.getWords();
-            WordsAdapter adapter = new WordsAdapter(SelectClassActivity.this);
+            WordsAdapter adapter = new WordsAdapter(getActivity());
             adapter.setItems(items);
-            listView.setAdapter(adapter);
+            setListAdapter(adapter);
 
             listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                 public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                    Intent intent = new Intent(getApplicationContext(), DrawInfoActivity.class);
+                    Intent intent = new Intent(getActivity().getApplicationContext(), DrawInfoActivity.class);
                     intent.putExtra("ID", items.get(position).getId());
                     startActivityForResult(intent, 1);      //DRAW_CODE = 1 / VIEW_CODEと被っている
                 }
@@ -72,12 +76,20 @@ public class SelectClassActivity extends AppCompatActivity {
     }
 
     @Override
+    public void onListItemClick(ListView listView, View view, int position, long id){
+        Intent intent = new Intent(getActivity().getApplicationContext(), SelectWordActivity.class);
+        intent.putExtra("CLASS", items[position]);
+        startActivityForResult(intent, VIEW_CODE);
+    }
+
+    /*
+    @Override
     public boolean onCreateOptionsMenu(Menu menu){
         //actionbarのカスタマイズ
-        final MenuInflater inflater = getMenuInflater();
+        final MenuInflater inflater = getActivity().getMenuInflater();
         inflater.inflate(R.menu.selectclasswords_menu, menu);
         return super.onCreateOptionsMenu(menu);
-    }
+    }*/
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
@@ -97,13 +109,13 @@ public class SelectClassActivity extends AppCompatActivity {
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode == RESULT_OK) {
+        if(resultCode == getActivity().RESULT_OK) {
             if(requestCode == VIEW_CODE){
                 if(data.getBooleanExtra("update", false))
                     DBAccess();
             }else if(requestCode == ADD_CODE)
                 //AddEditWordActivityより
-                if(resultCode == RESULT_OK)
+                if(resultCode == getActivity().RESULT_OK)
                     DBAccess();
         }
     }
